e0269327c7bed47df3dd5c0fa32f49d0364e06d2
Justin Mafie <60912277+Justin-Mafie@users.noreply.github.com>
2023-10-03 12:40:36 -0400

    Update qns_8.py

--- qns_8.py
+++ qns_8.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 # Create a dictionary with column names as keys and lists of column values as values
 data = {
-    'A': [3.1, 4.2, 1.5, 6.3],
-    'B': [800, 150, 400, 210],
-    'C': [1, 2, 2, 1]
+    'A': [1, 2, 2, 1],
+    'B': [3.1, 4.2, 1.5, 6.3],
+    'C': [800, 150, 400, 210]
 }
 
 # Create a DataFrame using the dictionary
